c9a9973dd21ca53d2d3da22425429a64d80dd279
EduardoLCavalcante <eduardocavalcante131@gmail.com>
2025-12-22 10:35:39 -0300

    update: removando calendario temporariamente

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -135,7 +135,7 @@ const CalendarSectionComponent = () => {
 
 
   return (
-    <section id="calendar" className="py-16 md:py-24 bg-black">
+    <section id="calendar" className="py-16 md:py-24 bg-black hidden">
       <div className="absolute w-full h-full z-0">
         <div className="absolute top-1/4 right-[20%] w-64 h-64 bg-maisvida-green rounded-full filter blur-[120px] opacity-20 animate-pulse"></div>
         <div className="absolute bottom-1/4 left-[20%] w-72 h-72 bg-maisvida-red rounded-full filter blur-[120px] opacity-20 animate-pulse" style={{ animationDelay: '1s' }}></div>
